ed34f8b25b500c552743afa62d3196dfa78d5853
igarashi kazuki <sousiki.links@gmail.com>
2024-12-12 18:01:37 +0900

    update: 余白の調整

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -13,7 +13,7 @@ export default function MainLayout() {
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="flex-1 p-8">
+      <main className="flex-1 p-6">
         <Outlet />
       </main>
     </div>
--- src/pages/PartsOrderPage.tsx
+++ src/pages/PartsOrderPage.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import OrderFormHeader from '../components/parts/OrderFormHeader';
-import OrderFormDetails from '../components/parts/OrderFormDetails';
-import OrderFormDelivery from '../components/parts/OrderFormDelivery';
-import OrderFormItems from '../components/parts/OrderFormItems';
-import OrderFormFooter from '../components/parts/OrderFormFooter';
-import OrderFormActions from '../components/parts/OrderFormActions';
+import React from "react";
+import OrderFormHeader from "../components/parts/OrderFormHeader";
+import OrderFormDetails from "../components/parts/OrderFormDetails";
+import OrderFormDelivery from "../components/parts/OrderFormDelivery";
+import OrderFormItems from "../components/parts/OrderFormItems";
+import OrderFormFooter from "../components/parts/OrderFormFooter";
+import OrderFormActions from "../components/parts/OrderFormActions";
 
 export default function PartsOrderPage() {
   const handleSubmit = (e: React.FormEvent) => {
@@ -17,11 +17,9 @@ export default function PartsOrderPage() {
       <div className="bg-white shadow rounded-lg">
         <div className="px-6 py-4 border-b border-gray-200 flex justify-between items-center">
           <h1 className="text-xl font-semibold text-gray-900">部品受注登録</h1>
-          <div className="text-sm text-gray-500">
-            処理日: 2015/04/23
-          </div>
+          <div className="text-sm text-gray-500">処理日: 2015/04/23</div>
         </div>
-        
+
         <form onSubmit={handleSubmit} className="p-6 space-y-6">
           <OrderFormHeader />
           <OrderFormDetails />
